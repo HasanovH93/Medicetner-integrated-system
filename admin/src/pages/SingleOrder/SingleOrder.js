69fe264a18921ab5a9a2da07266b7f7c10dbe589
HasanovH93 <hasanovh14@gmail.com>
2023-04-19 00:34:52 +0200

    single order styling

--- admin/src/pages/SingleOrder/SingleOrder.js
+++ admin/src/pages/SingleOrder/SingleOrder.js
@@ -6,48 +6,109 @@ import {
   Typography,
   Grid,
   Box,
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
 } from "@mui/material";
+import {
+  Email,
+  Phone,
+  Cake,
+  Home,
+  LocationOn,
+  MarkunreadMailbox,
+  ShoppingCart,
+  Receipt,
+  Spa,
+  Healing,
+} from "@mui/icons-material";
 
 const OrderCard = ({ order }) => {
-  console.log(order);
-  console.log("order", order.chosenCheckboxes);
+  const getItemIcon = (itemKey) => {
+    switch (itemKey) {
+      case "arbeitsunfähigkeitsbescheinigung":
+        return <Receipt />;
+      case "Heilmittel":
+        return <Spa />;
+      case "Medikamente":
+        return <Healing />;
+      default:
+        return <ShoppingCart />;
+    }
+  };
+
   return (
     <Card sx={{ minWidth: 700, maxWidth: 1200, mb: 4 }}>
       <CardHeader
+        avatar={
+          <Avatar>
+            {order.firstName.charAt(0)}
+            {order.secondName.charAt(0)}
+          </Avatar>
+        }
         title={`Order for ${order.firstName} ${order.secondName}`}
         subheader={`Order ID: ${order._id}`}
       />
+      <Divider />
       <CardContent>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            <Typography variant="h6">Contact Info:</Typography>
-            <Typography>Email: {order.email}</Typography>
-            <Typography>Phone: {order.phone}</Typography>
-            <Typography>Date of Birth: {order.dateofbirth}</Typography>
+            <Typography variant="h6" gutterBottom>
+              Contact Info:
+            </Typography>
+            <Typography>
+              <Email fontSize="small" /> Email: {order.email}
+            </Typography>
+            <Typography>
+              <Phone fontSize="small" /> Phone: {order.phone}
+            </Typography>
+            <Typography>
+              <Cake fontSize="small" /> Date of Birth: {order.dateofbirth}
+            </Typography>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Typography variant="h6">Address:</Typography>
-            <Typography>Street: {order.street}</Typography>
-            <Typography>City: {order.city}</Typography>
-            <Typography>Postcode: {order.postcode}</Typography>
+            <Typography variant="h6" gutterBottom>
+              Address:
+            </Typography>
+            <Typography>
+              <Home fontSize="small" /> Street: {order.street}
+            </Typography>
+            <Typography>
+              <LocationOn fontSize="small" /> City: {order.city}
+            </Typography>
+            <Typography>
+              <MarkunreadMailbox fontSize="small" /> Postcode: {order.postcode}
+            </Typography>
           </Grid>
         </Grid>
 
-        <Box mt={2}>
-          <Typography variant="h6">Chosen Items:</Typography>
-          {order.chosenCheckboxes.map((item, index) => {
-            const key = Object.keys(item)[0];
-            return (
-              <Box key={index} mt={1}>
-                <Typography variant="subtitle1">{key}:</Typography>
-                {Object.entries(item[key]).map(([subKey, value], idx) => (
-                  <Typography key={idx}>
-                    {subKey}: {value}
-                  </Typography>
-                ))}
-              </Box>
-            );
-          })}
+        <Box mt={3}>
+          <Typography variant="h6" gutterBottom>
+            Chosen Items:
+          </Typography>
+          <List>
+            {order.chosenCheckboxes.map((item, index) => {
+              const key = Object.keys(item)[0];
+              return (
+                <ListItem key={index}>
+                  <ListItemIcon>{getItemIcon(key)}</ListItemIcon>
+                  <ListItemText
+                    primary={key}
+                    secondary={Object.entries(item[key]).map(
+                      ([subKey, value], idx) => (
+                        <Typography key={idx} component="span" display="block">
+                          {subKey}: {value}
+                        </Typography>
+                      )
+                    )}
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
         </Box>
       </CardContent>
     </Card>
